db13317fe4204068828f4ab274035e0f53b91784
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-01-24 02:23:43 +0900

    feat: add sample docs
    Signed-off-by: AustinZhu <42071208+AustinZhu@users.noreply.github.com>

---
+++ lib/configs/api/doc.ts
@@ -0,0 +1,12 @@
+const sampleDoc = {
+    location: {
+        method: 'GET',
+        name: '',
+        path: '',
+        statusCode: '500',
+        type: 'error'
+    },
+    properties: JSON.stringify({
+        description: "The root resource with the default path specification."
+    })
+};
